931b24c03a938c1fba6c4131c2903497ea9a274f
Xianyx <pushkar44320@gmail.com>
2024-04-14 01:08:53 +0530

    attempt to send data to json??

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+/**
+ * @type HTMLInputElement
+ */
 function task_save(key_down) {
 
     if (event.key === 'Enter') {
@@ -5,7 +8,23 @@ function task_save(key_down) {
         console.log(key_down);
         alert(key_down.value)
     }
-    
+
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const data = new FormData(event.target);
+
+        // Do a bit of work to convert the entries to a plain JS object
+        const value = Object.fromEntries(data.entries());
+
+        console.log({ value });
+    }
+
+    const form = document.querySelector('form');
+    form.addEventListener('submit', handleSubmit);
+
 }
+
 
 console.log(task_save());
