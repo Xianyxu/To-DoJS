026feafd2bcdbb99e0c42ebb69a040dcf18f2c77
Xianyx <pushkar44320@gmail.com>
2024-04-15 01:18:42 +0530

    made some progress on the json part

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
+
+
 /**
  * @type HTMLInputElement
  */
-function task_save(key_down) {
+
+
+function input_taskHandler(key_down) {
 
     if (event.key === 'Enter') {
         var input = document.getElementById("input-task");
-        console.log(key_down);
-        alert(key_down.value)
+        console.log(key_down.value);
+
+        var input_data = {
+            "task_input:": input.value,
+        }
+        console.log(input_data);
+
+        let data = JSON.stringify(input_data);
     }
-
-
-    function handleSubmit(event) {
-        event.preventDefault();
-
-        const data = new FormData(event.target);
-
-        // Do a bit of work to convert the entries to a plain JS object
-        const value = Object.fromEntries(data.entries());
-
-        console.log({ value });
-    }
-
-    const form = document.querySelector('form');
-    form.addEventListener('submit', handleSubmit);
 
 }
+   
 
 
-console.log(task_save());
+
+
+
