3aee3436e79a1f6bc7cdb7365590657e3681d4ce
Xianyx <pushkar44320@gmail.com>
2024-04-25 17:52:36 +0530

    kinda works

---
+++ js_index.js
@@ -0,0 +1,8 @@
+const input_task = document.getElementById("input-task");
+const task_table = document.getElementsByClassName("task-table");
+
+function input_taskHandler(ele) {
+    if (event.key === 'Enter') {
+        
+    }
+}
