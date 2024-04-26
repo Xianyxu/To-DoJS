e03160667fe6766eb72dd5dcf058bcdcaf9baaa3
Xianyx <pushkar44320@gmail.com>
2024-04-26 10:55:25 +0530

    some more progress i guess

--- js_index.js
+++ js_index.js
@@ -1,8 +1,17 @@
-const input_task = document.getElementById("input-task");
-const task_table = document.getElementsByClassName("task-table");
+import fs from 'fs';
+
+
+const table_body = document.querySelectorAll(".table-body");
 
 function input_taskHandler(ele) {
+    const input_task_value = document.getElementById("input-task").value;
     if (event.key === 'Enter') {
-        
+        event.preventDefault();
+        table_body[0].innerHTML += `<div class="table-items">
+            ${input_task_value}
+        </div>`
     }
 }
+
+console.log(table_body[0].innerHTML);
+console.log();
