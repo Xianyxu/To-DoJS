7fc36cff56f183edeef6ec50c5734c4ac0496154
Xianyx <pushkar44320@gmail.com>
2024-04-28 17:28:35 +0530

    EventListener??

--- js_index.js
+++ js_index.js
@@ -2,16 +2,30 @@ import fs from 'fs';
 
 
 const table_body = document.querySelectorAll(".table-body");
+const input = document.getElementById("input-task")
 
-function input_taskHandler(ele) {
-    const input_task_value = document.getElementById("input-task").value;
+input.addEventListener("keypress", function (event) {
+    // 
     if (event.key === 'Enter') {
+        console.log(event.target.value);
         event.preventDefault();
         table_body[0].innerHTML += `<div class="table-items">
-            ${input_task_value}
+            ${event.target.value}
         </div>`
     }
-}
+
+})
+
+// function input_taskHandler(ele) {
+//     // const input_task_value = input.value;
+//     if (event.key === 'Enter') {
+//         console.log()
+//         event.preventDefault();
+//         table_body[0].innerHTML += `<div class="table-items">
+//             ${input_task_value}
+//         </div>`
+//     }
+// }
 
 console.log(table_body[0].innerHTML);
 console.log();
