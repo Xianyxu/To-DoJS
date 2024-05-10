12b093f8f88c8ade9bd20db97d65483cb6200ea4
Xianyx <pushkar44320@gmail.com>
2024-05-11 01:37:39 +0530

    no progress today. Just some tinkering here and there

--- js_index.js
+++ js_index.js
@@ -2,43 +2,28 @@ import fs from 'node:fs';
 // const json_file = require('./task_list.json')
 
 
-const table_body = document.querySelectorAll(".table-body");
+const table_body = document.querySelector(".table-body");
 const input = document.getElementById("input-task")
 const new_table_button = document.getElementById("new-table-btn")
 const tableItems = document.getElementsByClassName("table-items")
 const new_func = document.querySelector(".div-table-grid")
+const task_delete_button = document.querySelector(".task-delete-button")
 
-
-let i = 0
 
 input.addEventListener("keypress", function (event) {
     // 
     if (event.key === 'Enter') {
         // console.log(event.target.value);
         event.preventDefault();
-        table_body[0].innerHTML += /* html */ `
+        table_body.innerHTML += /* html */ `
         <div class="table-items">
             <p>${event.target.value}</p>
         </div>
         `
 
-        const json_object = {
-            "tasks": event.target.value
-        }
-
-        console.log(tableItems[i].innerHTML);
-
-        ++i
-
-        const data = JSON.stringify(json_object)
-        localStorage.setItem("task-list2", data)
-
     }
 
 })
-
-
-i += i
 
 
 new_table_button.addEventListener("click", () => {
@@ -50,7 +35,7 @@ new_table_button.addEventListener("click", () => {
                         <p>Tasks</p>
                     </div>
 
-                    <button>
+                    <button class="task-delete-button">
                         <img src="../resources/icons/SVG/trashbin.svg" height="30">
                     </button>
                 </div>
@@ -61,33 +46,38 @@ new_table_button.addEventListener("click", () => {
 })
 
 
-table_body[0].addEventListener('mouseover', () => {
+table_body.addEventListener('mouseover', () => {
 
-
-    let i = 0
-    i++
-
-    if (table_body[0].children[0].childElementCount <= 1) {
-        table_body[0].children[0].innerHTML += /* html */ `
+    if (table_body.children[0].childElementCount <= 1) {
+        table_body.children[0].innerHTML += /* html */ `
         <div class="table-items-onhover">
             <button >
                 <img src="/resources/icons/SVG/trash-can-solid.svg" height=15>
             </button>
         </div>
     `
+        
+        console.log(table_body.children[0].innerHTML)
     }
     
-    console.log(table_body[0].children[0].innerHTML)
+    
 
 })
 
-table_body[0].addEventListener("mouseout", () => {
+
+table_body.addEventListener("mouseout", () => {
     const remove_candidate = document.getElementsByClassName("table-items-onhover")
 
-    if (table_body[0].children[0].childElementCount > 1) {
+    if (table_body.children[0].childElementCount > 1) {
 
         remove_candidate[0].remove()
     }
 
 })
+
+task_delete_button.addEventListener("click", () => {
+    console.log(task_delete_button.parentElement)
+})
+
+
 
